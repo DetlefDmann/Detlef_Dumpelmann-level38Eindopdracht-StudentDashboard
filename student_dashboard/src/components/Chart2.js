3cd7207c40a1b0a3503ab9931f5c0e85a4bce685
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-05-14 10:58:16 +0200

    split up chart into components with comments

--- student_dashboard/src/components/Chart2.js
+++ student_dashboard/src/components/Chart2.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import './Chart.css'
 import { scaleBand , scaleLinear , line } from 'd3'; 
 import { useSelector } from 'react-redux';
-import { selectAverageArray, selectData, selectAssignmentsIsChecked, selectLoadingStatus } from "../features/studentData/studentDataSlice";
+import { selectAverageArray, selectData, selectAssignmentsIsChecked, selectLoadingStatus ,selectGraphOptions } from "../features/studentData/studentDataSlice";
 
 
 
 const Chart2 = ({ student }) => {
     const data = useSelector(selectData);
     const averages = useSelector(selectAverageArray);
+    const graphOptions = useSelector(selectGraphOptions);
     const [filteredData, setFilteredData] = useState(averages);
 
     let loadingStatus = "idle"
@@ -23,11 +24,7 @@ const Chart2 = ({ student }) => {
         }
         else if(loadingStatus==="ready"){
             
-            //         let newfilteredData = filteredData.filter(datapoint => {
-            //             return filterValues[datapoint.assignment]!==true
-            //         });
-        
-             console.log(filteredData)
+             console.log(graphOptions)
              setFilteredData(averages);
 
             //hier kunnen de methodes voor de home page komen
@@ -59,71 +56,112 @@ const Chart2 = ({ student }) => {
 
     const xOffset = width/filteredData.length*2 - innerWidth/filteredData.length*2        
 
-    return (
-        <div >
-        <svg width={width} height={height} className="chartcontainer" >
-            <g transform={`translate(${margin.left},${margin.top})`} className="chart">
-                {yScale.ticks(10).map(tickValue => (
-                    <g key={`${tickValue}y`} transform={`translate(0, ${yScale(tickValue)})`}>
-                        <line  x2={innerWidth} stroke="grey" strokeWidth="0.5"/>
-                        <text style={{textAnchor:"end"}} dy=".5em" x="-.5em">{tickValue}</text>
-                    </g>
-                ))};
-                <g  className="xTicks">
-                    {xScale.domain().map(tickValue => (
-                        <g key={`${tickValue}x`} transform={`translate(${xScale(tickValue) + innerWidth/(filteredData.length*2) -10},0) `}>
-                            <text transform={`rotate(315)`} style={{textAnchor:"end"}} x={-margin.left } y={innerHeight*Math.sqrt(2)}>{tickValue}</text>
-                        </g>
-                ))};
-                </g>
-                {filteredData.map((d,i) => (
-                    <g key={`${i}rect`}>
-                        <rect 
-                            key={i} 
-                            x={xScale(d.assignment) + xOffset/4} 
-                            y={innerHeight - d.funFactor*scaleToValues} 
-                            width={(innerWidth/(filteredData.length*2.5))} 
-                            height={d.funFactor*scaleToValues} 
-                            className="funFactor--bar"
-                            strokeWidth="0.25"
-                        >
-                            <title>{d.assignment} Fun factor: {(Math.round(d.funFactor*100))/100}</title>
-                        </rect>
-                        <rect 
-                            key={i+filteredData.length} 
-                            x={xScale(d.assignment) + innerWidth/(filteredData.length*2)} 
-                            y={ innerHeight-d.difficulty*scaleToValues} 
-                            width={(innerWidth/(filteredData.length*2.5))} 
-                            height={d.difficulty*scaleToValues} 
-                            className="difficulty--bar"
-                            strokeWidth="0.25"
-                            > <title>
-                                <title>
-                                    {d.assignment} <br />
-                                </title>
-                                <title>
-                                    Moeilijkheid: {(Math.round(d.difficulty*100))/100}
-                                </title>
-                            </title>
-                        </rect>
-                        <path className="difficulty--line"
-                            fill="none"
-                            stroke="black"
-                            strokeWidth="0.5"
-                            d={line()
-                                .x((d,i) => (i*innerWidth/filteredData.length)+(xOffset*i+innerWidth)/(filteredData.length*2))
-                                .y((d => innerHeight -d.difficulty*scaleToValues))(filteredData)}
-                        />
-                        <path className="funFactor--line"
+    //De y-as elementen
+    const YAxis = () => { 
+        return yScale.ticks(10).map(tickValue => (
+                <g key={`${tickValue}y`} transform={`translate(0, ${yScale(tickValue)})`}>
+                    <line  x2={innerWidth} stroke="grey" strokeWidth="0.5"/>
+                    <text style={{textAnchor:"end"}} dy=".5em" x="-.5em">{tickValue}</text>
+                </g>)
+                );
+    }
+
+    //De x-as elementen
+    const XAxis = () => {
+    return <g className="xTicks">
+            {xScale.domain().map(tickValue => (
+                <g key={`${tickValue}x`} transform={`translate(${xScale(tickValue) + innerWidth/(filteredData.length*2) -10},0) `}>
+                    <text transform={`rotate(315)`} style={{textAnchor:"end"}} x={-margin.left } y={innerHeight*Math.sqrt(2)}>{tickValue}</text>
+                </g>)
+            )}
+        </g>;
+    }
+
+    //De staafgrafiek voor leuk
+    const FunBarJSX = () => {
+        if ((graphOptions.show==="both"||graphOptions.show==="funFactor") && (graphOptions.graphType==="bar"||graphOptions.graphType==="lineAndBar")) {
+            return filteredData.map((d,i) => (
+                <rect 
+                    key={i} 
+                    x={xScale(d.assignment) + xOffset/4} 
+                    y={innerHeight - d.funFactor*scaleToValues} 
+                    width={(innerWidth/(filteredData.length*2.5))} 
+                    height={d.funFactor*scaleToValues} 
+                    className="funFactor--bar"
+                    strokeWidth="0.25"
+                >
+                    <title>{d.assignment} Fun factor: {(Math.round(d.funFactor*100))/100}</title>
+                </rect>));
+        }
+        else return null;
+    }
+
+    //De staafgrafiek voor moeilijk
+    const DiffBarJSX = () => {
+        if ((graphOptions.show==="both"||graphOptions.show==="difficulty") && (graphOptions.graphType==="bar"||graphOptions.graphType==="lineAndBar")){
+        return filteredData.map((d,i) => (
+                <rect 
+                    key={i+filteredData.length} 
+                    x={xScale(d.assignment) + innerWidth/(filteredData.length*2)} 
+                    y={ innerHeight-d.difficulty*scaleToValues} 
+                    width={(innerWidth/(filteredData.length*2.5))} 
+                    height={d.difficulty*scaleToValues} 
+                    className="difficulty--bar"
+                    strokeWidth="0.25"
+                    > <title>
+                        <title>
+                            {d.assignment} <br />
+                        </title>
+                        <title>
+                            Moeilijkheid: {(Math.round(d.difficulty*100))/100}
+                        </title>
+                    </title>
+                </rect>));
+        }
+        else return null;
+    };
+
+    //De lijngrafiek voor leuk
+    const FunLineJSX = () => {
+        if ((graphOptions.show==="both"||graphOptions.show==="funFactor") && (graphOptions.graphType==="line"||graphOptions.graphType==="lineAndBar")){
+        return (<path className="funFactor--line"
                             fill="none"
                             stroke="red"
                             strokeWidth="0.5"
                             d={line()
                                 .x((d,i) => (i*innerWidth/filteredData.length)+(xOffset*i+innerWidth)/(filteredData.length*2))
                                 .y((d => innerHeight -d.funFactor*scaleToValues))(filteredData)}
-                        />
-                    </g>))
-                }
+                        />);
+        }
+        else return null;
+}
+    //De lijngrafiek voor moeilijk
+    const DiffLineJSX = () => {
+        if ((graphOptions.show==="both"||graphOptions.show==="difficulty") && (graphOptions.graphType==="line"||graphOptions.graphType==="lineAndBar")){
+        return <path className="difficulty--line"
+                            fill="none"
+                            stroke="black"
+                            strokeWidth="0.5"
+                            d={line()
+                                .x((d,i) => (i*innerWidth/filteredData.length)+(xOffset*i+innerWidth)/(filteredData.length*2))
+                                .y((d => innerHeight -d.difficulty*scaleToValues))(filteredData)}
+                        />;
+        }
+        else return null;
+}
+
+
+    return (
+        <div >
+        <svg width={width} height={height} className="chartcontainer" >
+            <g transform={`translate(${margin.left},${margin.top})`} className="chart">
+                <YAxis />
+                <XAxis />
+                <FunBarJSX />
+                <DiffBarJSX />
+                <FunLineJSX />
+                <DiffLineJSX />
+                <text></text>
             </g>
         </svg>
         </div>
