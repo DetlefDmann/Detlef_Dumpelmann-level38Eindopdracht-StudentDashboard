72ba9cee47b1e6a2c85895e0c41dc28accb5ec00
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-05-16 20:52:31 +0200

    prepare students extra info

--- student_dashboard/src/features/studentData/StudentData.js
+++ student_dashboard/src/features/studentData/StudentData.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Chart2 from "../../components/Chart2";
-import GraphSelectors from '../../components/GraphSelectors'
+import GraphSelectors from '../../components/GraphSelectors';
+import StudentStats from '../../components/StudentStats'
+import StudentProfile from "../../components/StudentProfile";
 
 const StudentData = ({student}) => {
     // Hier willen we wat informatie over de student neer zetten,
     //wanneer we op de homepage zijn willen we juist algemene informatie weergeven.
 
     return (
-        <main>
-            <h1>Dit is de pagina van {student}</h1>
-            <Chart2 student={student}/>
-            <GraphSelectors />
-        </main>
+        <>
+            <StudentProfile student={student} />
+            <StudentStats />
+            <main>
+                <h1>Dit is de pagina van {student}</h1>
+                <Chart2 student={student}/>
+                <GraphSelectors />
+            </main>
+        </>
     )
 }
 
